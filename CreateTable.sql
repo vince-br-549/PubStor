c16b14e26c10c3a5cf8cebd302079d8176d214ab
vince-br-549 <vince@planetvince.com>
2018-01-13 16:15:10 -0500

    Add sql statements to create example table

---
+++ CreateTable.sql
@@ -0,0 +1,83 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: shelf; Type: TABLE; Schema: public; Owner: vince; Tablespace: 
+--
+
+CREATE TABLE shelf (
+    id integer NOT NULL,
+    title character varying(200),
+    author character varying(200),
+    publisher character varying(200),
+    isbn character varying(200),
+    genre character varying(200)
+);
+
+
+ALTER TABLE public.shelf OWNER TO vince;
+
+--
+-- Name: shelf_id_seq; Type: SEQUENCE; Schema: public; Owner: vince
+--
+
+CREATE SEQUENCE shelf_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.shelf_id_seq OWNER TO vince;
+
+--
+-- Name: shelf_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: vince
+--
+
+ALTER SEQUENCE shelf_id_seq OWNED BY shelf.id;
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: vince
+--
+
+ALTER TABLE ONLY shelf ALTER COLUMN id SET DEFAULT nextval('shelf_id_seq'::regclass);
+
+
+--
+-- Data for Name: shelf; Type: TABLE DATA; Schema: public; Owner: vince
+--
+
+COPY shelf (id, title, author, publisher, isbn, genre) FROM stdin;
+1	Book1	Vince	none	1	fiction
+2	 Book2	Vince	vince	1	None 
+3	 Book3	Vince	vince		 
+4	 BookG	Joe	vince	0	none 
+\.
+
+
+--
+-- Name: shelf_id_seq; Type: SEQUENCE SET; Schema: public; Owner: vince
+--
+
+SELECT pg_catalog.setval('shelf_id_seq', 4, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
--- README.txt
+++
@@ -1,5 +0,0 @@
-Thu Jan 11 22:26:27 EST 2018
-see
-https://apparatusd.wordpress.com/2010/09/08/simple-qt4-and-postgresql-book-catalog-application/
-
-
